2ad2d836ff62863776f2d3e9317d40b34be11512
mango <mchead1@naver.com>
2021-10-05 15:12:11 +0900

    :package: Compile the product package file

--- main.go
+++ main.go
@@ -1,15 +1,56 @@
-package study_grpc_server
+package main
 
 import (
 	"context"
+	"github.com/gofrs/uuid"
 	"google.golang.org/grpc"
-	pb "github.com/Sujin1135/study-grpc-idl"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"log"
+	"net"
+	pb "study-grpc-server/proto"
 )
 
-func (s *server) AddProduct(ctx context.Context, in *pb.Product) {
-	// Write your business code
+const (
+	port = ":50051"
+)
+
+type server struct {
+	productMap map[string]*pb.Product
 }
 
-func (s *server) GetProduct(ctx context.Context, in *pb.ProductId) {
+func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
+	out, err := uuid.NewV4()
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Error while generating Product ID", err)
+	}
+	in.Id = out.String()
+	if s.productMap == nil {
+		s.productMap = make(map[string]*pb.Product)
+	}
+	s.productMap[in.Id] = in
+	log.Printf("Product %v : %v - Added.", in.Id, in.Name)
+	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
+}
+
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
 	// Write your business code
+	product, exists := s.productMap[in.Value]
+	if exists && product != nil {
+		log.Printf("Product %v : %v - Retrieved.", product.Id, product.Name)
+		return product, status.New(codes.OK, "").Err()
+	}
+	return nil, status.Errorf(codes.NotFound, "Product does not exist.", in.Value)
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterProductInfoServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
