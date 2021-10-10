6d323d130e5b12741901c992de28b4e4fabd01a3
chris <mchead1@naver.com>
2021-10-11 02:41:18 +0900

    :sparkles: practice bidirectional stream of grpc

--- order_service.go
+++ order_service.go
@@ -9,6 +9,10 @@ import (
 	pb "study-grpc-server/order/order"
 )
 
+const (
+	orderBatchSize = 3
+)
+
 var orderMap = make(map[string]pb.Order)
 
 type orderServer struct {
@@ -54,4 +58,55 @@ func initSampleData() {
 	orderMap["104"] = pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub"}, Destination: "Mountain View, CA", Price: 400.00}
 	orderMap["105"] = pb.Order{Id: "105", Items: []string{"Amazon Echo"}, Destination: "San Jose, CA", Price: 30.00}
 	orderMap["106"] = pb.Order{Id: "106", Items: []string{"Amazon Echo", "Apple iPhone XS"}, Destination: "Mountain View, CA", Price: 300.00}
+}
+
+func (s *orderServer) ProcessOrders(stream pb.OrderManagement_ProcessOrdersServer) error {
+	batchMarker := 1
+	combinedShipmentMap := make(map[string]pb.CombinedShipment)
+
+	for {
+		orderId, err := stream.Recv()
+		log.Printf("Reading Proc order: %s", orderId)
+		if err == io.EOF {
+			log.Printf("EOF : %s", orderId)
+			for _, comb := range combinedShipmentMap {
+				log.Fatalf("comb: %v", comb)
+				if err := stream.Send(&comb); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		// business logics as below
+		destination := orderMap[orderId.GetValue()].Destination
+		shipment, found := combinedShipmentMap[destination]
+
+		if found {
+			ord := orderMap[orderId.GetValue()]
+			shipment.OrderList = append(shipment.OrderList, &ord)
+			combinedShipmentMap[destination] = shipment
+		} else {
+			comShip := pb.CombinedShipment{Id: "cmb - " + (orderMap[orderId.GetValue()].Destination), Status: "Processed!"}
+			ord := orderMap[orderId.GetValue()]
+			comShip.OrderList = append(shipment.OrderList, &ord)
+			combinedShipmentMap[destination] = comShip
+			log.Print(len(comShip.OrderList), comShip.GetId())
+		}
+
+		// end business logics
+
+		if batchMarker == orderBatchSize {
+			for _, comb := range combinedShipmentMap {
+				stream.Send(&comb)
+			}
+			batchMarker = 0
+			combinedShipmentMap = make(map[string]pb.CombinedShipment)
+		} else {
+			batchMarker++
+		}
+	}
 }
