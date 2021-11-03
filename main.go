0e39655e37a0b9bba47ed7d90a0db8b2ec0e5ed1
chris <mchead1@naver.com>
2021-11-04 01:08:14 +0900

    :sparkles: TLS 보안 연결(로컬 테스트 용도 OpenSSL)

--- main.go
+++ main.go
@@ -1,19 +1,46 @@
 package main
 
-import "sync"
+import (
+	"crypto/tls"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
+	"net"
+	pb "study-grpc-server/ecommerce/ecommerce"
+)
 
 const (
-	port = ":50051"
+	port    = ":50051"
+	crtFile = "server.crt"
+	keyFile = "server.key"
 )
 
 func main() {
-	var wg sync.WaitGroup
-	for _, addr := range addrs {
-		wg.Add(1)
-		go func(addr string) {
-			defer wg.Done()
-			startServer(addr)
-		}(addr)
+	// 1. Generate a certification
+	cert, err := tls.LoadX509KeyPair(crtFile, keyFile)
+	if err != nil {
+		log.Fatalf("failed to load key pair: %s", err)
 	}
-	wg.Wait()
+
+	// 2. Enable the certification
+	opts := []grpc.ServerOption{
+		grpc.Creds(credentials.NewServerTLSFromCert(&cert)),
+	}
+
+	// 3. Create a gRPC server instance with the TLS certification
+	s := grpc.NewServer(opts...)
+
+	// 4. Register a gRPC server into the created gRPC server
+	pb.RegisterProductInfoServer(s, &server{})
+
+	// 5. Create a listener with port(50051)
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// 6. Bind the gRPC server into the created listener
+	if err := s.Serve(lis); err != nil { // 6
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
